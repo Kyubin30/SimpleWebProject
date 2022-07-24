69471a303618b9f3fe4f6c9c5cfa02709936495d
Kyubin30 <leekebin3@naver.com>
2022-07-24 21:14:15 +0900

    라우터 이동

--- nuna/src/components/Login.vue
+++ nuna/src/components/Login.vue
@@ -4,7 +4,7 @@
     <input type="text" placeholder="email"><br>
     <input type="password" placeholder="password"><br>
     <button>로그인</button>
-    <p>만약 계정이 없다면, 회원가입을 먼저 진행해주세요!</p>
+    <p>만약 계정이 없다면, <router-link to="/signup">회원가입</router-link>을 먼저 진행해주세요!</p>
   </div>
 </template>
  
