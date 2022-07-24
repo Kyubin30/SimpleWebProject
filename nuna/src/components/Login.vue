65499e831c3a768f0d2740fd8e358d5bf1b74f03
Kyubin30 <leekebin3@naver.com>
2022-07-24 21:18:26 +0900

    로그인 버튼 구현

--- nuna/src/components/Login.vue
+++ nuna/src/components/Login.vue
@@ -3,7 +3,7 @@
     <h3>Login</h3>
     <input type="text" placeholder="email"><br>
     <input type="password" placeholder="password"><br>
-    <button>로그인</button>
+    <button v-on:click="login">로그인</button>
     <p>만약 계정이 없다면, <router-link to="/signup">회원가입</router-link>을 먼저 진행해주세요!</p>
   </div>
 </template>
@@ -15,7 +15,11 @@
       return {
       }
     },
-    methods: {}
+    methods: {
+        login() {
+        this.$router.replace('hello')
+        }
+    }
   }
 </script>
  
